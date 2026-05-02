ca3027da9e4bdd1d405dfb2be6757bbc70801cde
NehaBansal451 <bansalneha451@gmail.com>
2026-05-02 23:11:16 +0530

    connected live ml

--- public/js/main.js
+++ public/js/main.js
@@ -297,7 +297,7 @@ async function getHealth(){
   console.log("Sending:", patient.age, avgGlucose); // 🔥 DEBUG
 
   // ✅ STEP 2: call ML API
-  const response = await fetch("http://127.0.0.1:5000/predict", {
+  const response = await fetch("https://ml-diabetes-api.onrender.com/predict", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"

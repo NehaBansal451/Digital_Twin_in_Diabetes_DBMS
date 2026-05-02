14940dc8ba84429b80876f824a837cf6adc36b0e
NehaBansal451 <bansalneha451@gmail.com>
2026-05-02 21:36:59 +0530

    reconnect database

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,   // ← changed this
-  port: parseInt(process.env.MYSQLPORT)
+  uri: process.env.MYSQL_URL
 });
 
 module.exports = db;

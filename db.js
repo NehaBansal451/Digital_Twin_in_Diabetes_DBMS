f4eb4969fa286231c010c639c3f692a523e24b62
NehaBansal451 <bansalneha451@gmail.com>
2026-05-02 21:27:26 +0530

    fix db connection for Railway

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT
 });
 
-module.exports = pool.promise();
+module.exports = db;
